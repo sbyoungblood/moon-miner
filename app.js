fe3cd9bb677edec7c715c292f7a0fe967c18345e
Scotty Youngblood <scottybyoungblood@gmail.com>
2023-01-27 12:14:49 -0700

    mine function is wired

---
+++ app.js
@@ -0,0 +1,17 @@
+// SECTION variables
+
+let dust = 0
+
+// SECTION draw functions
+
+function drawDust(){
+  
+}
+
+// SECTION additional functions
+
+function mine(){
+  dust += 1
+  console.log(dust);
+
+}
