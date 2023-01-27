3728bb75afb35d99d2997ee3c260f997f8983c30
Scotty Youngblood <scottybyoungblood@gmail.com>
2023-01-27 15:52:49 -0700

    styling

--- app.js
+++ app.js
@@ -2,10 +2,64 @@
 
 let dust = 0
 
+// SECTION arrays
+
+let clickUpgrades = [
+  {
+    name: 'pickaxe',
+    price: 100,
+    quantity: 0,
+    multiplier: 1
+  },
+  {
+    name: 'drill',
+    price: 250,
+    quantity: 0,
+    multiplier: 5
+  },
+  {
+    name: 'rover',
+    price: 500,
+    quantity: 0,
+    multiplier: 10
+  }
+];
+
+let automaticUpgrades = [
+  {
+    name: 'astronaut',
+    price: 1000,
+    quantity: 0,
+    multiplier: 20
+  },
+  {
+    name: 'habitat',
+    price: 5000,
+    quantity: 0,
+    multiplier:50 
+  },
+  {
+    name: 'colony',
+    price: 10000,
+    quantity: 0,
+    multiplier: 100
+  }
+];
+
 // SECTION draw functions
 
 function drawDust(){
-  
+  let dustElem = document.getElementById('total-dust')
+
+  let template = ''
+
+  template += /*html*/
+  `
+  <div class="card text-center display-6 d-flex align-items-center">
+  <div>Dust: ${dust}</div>
+  </div>
+  `
+  dustElem.innerHTML = template
 }
 
 // SECTION additional functions
@@ -13,5 +67,7 @@ function drawDust(){
 function mine(){
   dust += 1
   console.log(dust);
-
+  drawDust()
 }
+
+
