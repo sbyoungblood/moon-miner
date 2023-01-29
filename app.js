60808e3fd29b3efac640bcfac4790ace9c59805f
Scotty Youngblood <scottybyoungblood@gmail.com>
2023-01-29 03:30:08 -0700

    finished product

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 // SECTION variables
 
-let dust = 0
+let totalDust = 0
+let click = 1
+let clickTotal = 0
+let intervalTotal = 0
 
 // SECTION arrays
 
-let clickUpgrades = [
+let upgrades = [
   {
     name: 'shovel',
     price: 100,
@@ -12,7 +15,7 @@ let clickUpgrades = [
     multiplier: 1
   },
   {
-    name: 'bomb',
+    name: 'explosives',
     price: 250,
     quantity: 0,
     multiplier: 5
@@ -25,7 +28,7 @@ let clickUpgrades = [
   }
 ];
 
-let automaticUpgrades = [
+let automation = [
   {
     name: 'astronaut',
     price: 1000,
@@ -34,13 +37,13 @@ let automaticUpgrades = [
   },
   {
     name: 'habitat',
-    price: 5000,
+    price: 2500,
     quantity: 0,
-    multiplier:50 
+    multiplier: 50 
   },
   {
     name: 'colony',
-    price: 10000,
+    price: 5000,
     quantity: 0,
     multiplier: 100
   }
@@ -49,7 +52,7 @@ let automaticUpgrades = [
 // SECTION draw functions
 
 function drawDust(){
-  let dustElem = document.getElementById('total-dust')
+  let totalDustElem = document.getElementById('total-dust')
 
   // let template = ''
 
@@ -61,19 +64,190 @@ function drawDust(){
   // `
   // dustElem.innerHTML = template
 
-  dustElem.innerText = dust
+  totalDustElem.innerText = `${totalDust.toFixed(0)}`
+}
+
+function drawClickTotal(){
+  let shovel = upgrades.find(upgrade => upgrade.name == 'shovel')
+  let shovelClickTotal = shovel.multiplier * shovel.quantity
+  console.log(shovelClickTotal);
+
+  let explosives = upgrades.find(upgrade => upgrade.name == 'explosives')
+  let explosivesClickTotal = explosives.multiplier * explosives.quantity
+  console.log(explosivesClickTotal);
+
+  let rover = upgrades.find(upgrade => upgrade.name == 'rover')
+  let roverClickTotal = rover.multiplier * rover.quantity
+  console.log(roverClickTotal);
+  
+  clickTotal = shovelClickTotal + explosivesClickTotal + roverClickTotal + click
+  console.log(clickTotal);
+
+  let clickTotalElem = document.getElementById('click-total')
+  clickTotalElem.innerHTML = `${clickTotal}`
+}
+
+
+
+function drawIntervalTotal(){
+
+  let astronaut = automation.find(automation => automation.name == 'astronaut')
+  let astronautIntervalTotal = astronaut.multiplier * astronaut.quantity
+
+  console.log(astronautIntervalTotal);
+
+  let habitat = automation.find(automation => automation.name == 'habitat')
+  let habitatIntervalTotal = habitat.multiplier * habitat.quantity
+
+  console.log(habitatIntervalTotal);
+
+
+
+  let colony = automation.find(automation => automation.name == 'colony')
+  let colonyIntervalTotal = colony.multiplier * colony.quantity
+
+  intervalTotal = astronautIntervalTotal + habitatIntervalTotal + colonyIntervalTotal
+
+  let intervalTotalElem = document.getElementById('interval-total')
+  intervalTotalElem.innerHTML = `${intervalTotal}`
+
+}
+
+function drawAstronautsTotal(){
+  let astronautTotalElem = document.getElementById('total-astronauts')
+  let astronautPriceElem = document.getElementById('astronaut-price')
+  
+  let astronautTotal = automation.find(automation => automation.name == 'astronaut')
+  let astronautPrice = automation.find(automation => automation.name == 'astronaut')
+  
+  astronautTotalElem.innerHTML = ` ${astronautTotal.quantity}`
+  astronautPriceElem.innerHTML = ` ${astronautPrice.price.toFixed(0)}`
+}
+
+function drawHabitatTotal(){
+  let habitatTotalElem = document.getElementById('total-habitats')
+  let habitatPriceElem = document.getElementById('habitat-price')
+  
+  let habitatTotal = automation.find(automation => automation.name == 'habitat')
+  let habitatPrice = automation.find(automation => automation.name == 'habitat')
+  
+  habitatTotalElem.innerHTML = ` ${habitatTotal.quantity}`
+  habitatPriceElem.innerHTML = ` ${habitatPrice.price.toFixed(0)}`
+}
+
+
+function drawColonyTotal(){
+  let colonyTotalElem = document.getElementById('total-colonies')
+  let colonyPriceElem = document.getElementById('colony-price')
+  
+  let colonyTotal = automation.find(automation => automation.name == 'colony')
+  let colonyPrice = automation.find(automation => automation.name == 'colony')
+  
+  colonyTotalElem.innerHTML = ` ${colonyTotal.quantity}`
+  colonyPriceElem.innerHTML = ` ${colonyPrice.price.toFixed(0)}`
+}
+
+function drawAutomationTotals(){
+  drawAstronautsTotal()
+  drawHabitatTotal()
+  drawColonyTotal()
+}
+
+
+function drawShovelTotal(){
+  let shovelTotalElem = document.getElementById('total-shovels')
+  let shovelPriceElem = document.getElementById('shovel-price')
+  
+  let shovelTotal = upgrades.find(upgrade => upgrade.name == 'shovel')
+  let shovelPrice = upgrades.find(upgrade => upgrade.name == 'shovel')
+  
+  shovelTotalElem.innerHTML = ` ${shovelTotal.quantity}`
+  shovelPriceElem.innerHTML = ` ${shovelPrice.price.toFixed(0)}`
+}
+
+function drawExplosivesTotal(){
+  let explosivesTotalElem = document.getElementById('total-explosives')
+  let explosivesPriceElem = document.getElementById('explosives-price')
+
+  let explosivesTotal = upgrades.find(upgrade => upgrade.name == 'explosives')
+  let explosivesPrice = upgrades.find(upgrade => upgrade.name == 'explosives')
+  
+  explosivesTotalElem.innerHTML = ` ${explosivesTotal.quantity}`
+  explosivesPriceElem.innerHTML = ` ${explosivesPrice.price.toFixed(0)}`
+}
+
+function drawRoversTotal(){
+  let roversTotalElem = document.getElementById('total-rovers')
+  let roversPriceElem = document.getElementById('rover-price')
+
+  let roversTotal = upgrades.find(upgrade => upgrade.name == 'rover')
+  let roversPrice = upgrades.find(upgrade => upgrade.name == 'rover')
+  
+  roversTotalElem.innerHTML = ` ${roversTotal.quantity}`
+  roversPriceElem.innerHTML = ` ${roversPrice.price.toFixed(0)}`
+}
+
+function drawUpgradeTotals(){
+  drawShovelTotal()
+  drawExplosivesTotal()
+  drawRoversTotal()
 }
 
 // SECTION additional functions
 
+// function mine(){
+//   totalDust++
+//   drawDust()
+// }
+
 function mine(){
-  dust += 1
-  console.log(dust);
+  upgrades.forEach(upgrade =>{
+    totalDust += (upgrade.quantity * upgrade.multiplier)
+  })
+  totalDust++
   drawDust()
 }
 
-function buyShovel(){
-  
+function mineInterval(){
+  automation.forEach(automation =>{
+    totalDust += (automation.quantity * automation.multiplier)
+  })
+  drawDust()
 }
 
+function purchaseUpgrade(upgradeName){
+  let purchasedUpgrade = upgrades.find(upgrade => upgrade.name == upgradeName)
+  
+  
+  if(totalDust >= purchasedUpgrade.price){
+    totalDust -= purchasedUpgrade.price
+    purchasedUpgrade.quantity++
+    purchasedUpgrade.price += purchasedUpgrade.price * .20
+  }else{
+      window.alert("Oof. You're practically broke. Mine the moon!")
+    }
+  
+  // console.log(purchasedUpgrade);
+  drawClickTotal()
+  drawUpgradeTotals()
+  drawDust()
+}
+function purchaseAutomation(automationName){
+  let purchasedAutomation = automation.find(automation => automation.name == automationName)
+  
+  if(totalDust >= purchasedAutomation.price){
+    totalDust -= purchasedAutomation.price
+    purchasedAutomation.quantity++
+    purchasedAutomation.price += purchasedAutomation.price * .30
+  }else{
+    window.alert("Oof. You're practically broke. Mine the moon!")
+  }
+  // console.log(purchasedAutomation);
+  drawIntervalTotal()
+  drawAutomationTotals()
+  drawDust()
+}
 
+drawDust()
+
+setInterval(mineInterval, 3000)
